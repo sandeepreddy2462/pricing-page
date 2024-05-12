252be1a744a5346bafd18e62d792a20b39ac963b
Sandeep Reddy <157559130+sandeepreddy2462@users.noreply.github.com>
2024-05-12 16:50:29 +0530

    script.js

--- pricing page/script.js
+++ pricing page/script.js
@@ -23,4 +23,30 @@ function setInitialPrices() {
 }
 
 setInitialPrices();
-priceUpdate();
+
+
+function updateCurrency() {
+  const currencySelect = document.getElementById("currency-select").value;
+
+  pricingcards.forEach(card => {
+    let priceElement = card.querySelector(".price");
+    let initialPrice = parseFloat(priceElement.dataset.initial);
+
+    switch (currencySelect) {
+      case "usd":
+        priceElement.textContent = '$' + initialPrice.toFixed(2);
+        break;
+      case "inr":
+        priceElement.textContent = '₹' + (initialPrice * 75.5).toFixed(2);
+        break;
+      case "aed":
+        priceElement.textContent = 'د.إ' + (initialPrice * 3.67).toFixed(2);
+        break;
+      default:
+        priceElement.textContent = '$' + initialPrice.toFixed(2);
+    }
+  });
+}
+
+const currencySelect = document.getElementById("currency-select");
+currencySelect.addEventListener('change', updateCurrency);
